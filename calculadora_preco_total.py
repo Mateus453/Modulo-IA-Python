35f87215d6cd32da9975cb3d724ad8ce2d1a36d6
Mateus453 <mateusspbr28@gmail.com>
2025-09-08 22:28:52 -0300

    Concluido 3 novos exercicios

---
+++ calculadora_preco_total.py
@@ -0,0 +1,10 @@
+# Este programa calcula o preço total de uma compra
+
+# Solicita informações sobre o produto ao usuário
+nome_produto = "cadeira infantil"
+preco_unitario = 12.40
+quantidade = 3
+
+preco_total = preco_unitario * quantidade
+
+print(preco_total)

dd6b6d0dba2389271f542ccabec7c9e03e9ef39d
Mateus453 <mateusspbr28@gmail.com>
2025-09-12 19:25:24 -0300

    Entrada dinâmica dos dados com a função input e a formatação com f-string

--- atividade-pratica1/calculadora_preco_total.py
+++ atividade-pratica1/calculadora_preco_total.py
@@ -1,10 +1,15 @@
 # Este programa calcula o preço total de uma compra
 
 # Solicita informações sobre o produto ao usuário
-nome_produto = "cadeira infantil"
-preco_unitario = 12.40
-quantidade = 3
+nome_produto = input("Digite o nome do produto:" )
+preco_unitario = float(input("Digite o valor do produto: "))
+quantidade = int(input("Digite a quantidade de produtos:" ))
 
+#Calculadora de preço total
 preco_total = preco_unitario * quantidade
 
-print(preco_total)
+#Exibe o resultado
+print(f"\nProduto: {nome_produto}")
+print(f"Preco unitário: R$ {preco_unitario:.2f}")
+print(f"Quantidade: {quantidade}")
+print(f"Total: R$ {preco_total:.2f}")
